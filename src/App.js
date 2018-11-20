e726e335a252d35cb3e5c5e06ad2901ea7a1b625
rivasd <rivasdaniel1992@gmail.com>
2018-11-19 21:40:31 -0500

    css crap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import Bandit from './Bandit'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main className="App-main">
+
+          <div style={ {margin : 'auto auto'}}>
+            <Bandit></Bandit>
+            <button id="spin">Spin moi ça !</button>
+          </div>
+          
+
+        </main>
+        <footer className="App-footer">
+          Un autre produit de marque EZ
+
+        </footer>
       </div>
     );
   }
---
+++ src/Bandit.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './App.css'
+import SlotMachine from 'jquery-slotmachine/dist/slotmachine'
+
+
+class Bandit extends Component{
+
+    render() {
+        return <div className="bandit"></div> 
+    }
+}
+
+export default Bandit
