5d9fe4fe862771100670b860bb0dcd3a91a3e135
rivasd <rivasdaniel1992@gmail.com>
2018-11-20 01:25:28 -0500

    seems working??

--- src/Bandit.js
+++ src/Bandit.js
@@ -11,22 +11,26 @@ class Bandit extends Component{
         this.machines = [];
         this.counter = 0;
         this.spin = this.spin.bind(this);
+        this.stop = this.stop.bind(this);
     }
     
     spin(){
         this.counter = this.props.icons.length;
         this.machines.map((machine, idx) => {
-            machine.shuffle(9999);
+            machine.shuffle(9999999);
         });
+    }
+
+    stop(){
+        if (this.counter > 0){
+            this.machines[this.counter-1].stop();
+            this.counter--;
+        }
     }
 
     render() {
 
-        var iconstyle= {
-            height: "100px",
-            width:"100%",
-            align: "middle"
-        }
+        
 
         var btnstyle= {
             display: "block",
@@ -40,13 +44,17 @@ class Bandit extends Component{
                         {this.props.icons.map( (icongroup, i) => 
                             
                             <div key={"scroll-"+i} className="slotMachine" ref={(instance) =>this.slots[i] = instance}>
-                                {icongroup.map( (icon, idx) => <img className="slot" key={"icon"+idx} src={"img/"+icon} style={iconstyle} />)}
+                                {icongroup.map( (icon, idx) => <div className="slot" key={"icon"+idx} style={{backgroundImage:"url('img/"+icon+"')"}}></div>)}
                             </div>
                         )}
                     </div>
                     
                 </div>
-                <button onClick={this.spin} style={btnstyle}>Spin moi ça!</button>
+                <div style={btnstyle}>
+                    <button onClick={this.spin} >Spin moi ça!</button>
+                    <button onClick={this.stop}> Wo de Wo!</button>
+                </div>
+                
             </div> )
     }
 
@@ -56,9 +64,9 @@ class Bandit extends Component{
             var machine = new SlotMachine(ref, {
                 active: i,
                 delay:500
-            })
+            });
             this.machines.push(machine)
-        })
+        });
     }
 }
 
